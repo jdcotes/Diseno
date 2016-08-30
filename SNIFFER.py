419e7011538c8571856e086016b2697cc4ce9643
jdcotes <jdcotes@uninorte.edu.co>
2016-08-29 23:33:06 -0500

    Actualizacion 0.1
    Archivos agregados. Página web.

---
+++ SNIFFER.py
@@ -0,0 +1,95 @@
+import socket
+import datetime
+import mysql.connector
+
+IP = "192.168.1.250"
+PORT = 9000
+numero_mensaje = 1
+latnew=0
+longnew=0
+minnew=0
+print("Sniffer Address: %s" % IP)
+print("Puerto: %.0f" % PORT)
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind((IP,PORT))
+
+while True:
+    data,addr=sock.recvfrom(1024)
+    Fecha_captura = datetime.datetime.now()
+
+    print("--------------------------------")
+    print("Mensaje N°: %i" % numero_mensaje)
+    numero_mensaje = numero_mensaje + 1
+    
+    if len(data) == 63:
+        
+        data = str(data)
+    
+        Anio = str(Fecha_captura.year)
+        Mes = str(Fecha_captura.month)
+        if len(Mes) < 2:
+            Mes = "0%s" % Mes
+        Dia = str(Fecha_captura.day)
+        if len(Dia) < 2:
+            Dia = "0%s" % Dia
+        
+        Hora = str(Fecha_captura.hour)
+        if len(Hora) < 2:
+            Hora = "0%s" % Hora
+        Mins = str(Fecha_captura.minute)
+        minutes=float(Fecha_captura.minute)
+        if len(Mins) < 2:
+            Mins = "0%s" % Mins
+        Secs = str(Fecha_captura.second)
+        if len(Secs) < 2:
+            Secs = "0%s" % Secs
+
+   #    lat = str(float(data[19:26])/100000)
+   #    titud=float(data[20:26])/100000           
+        titud=float(data[20:26])/1000
+        lat=str(titud)
+    #   long = str(float(data[28:35])/-100000) 
+    #   gtitud=float(data[28:35])/-100000
+        gtitud=float(data[28:35])/1000
+        long=str(gtitud)
+        text_file=open("Fecha.txt","w")
+        text_file.write("%s/%s/%s" %(Anio,Mes,Dia))
+        text_file.close()
+
+        text_file=open("Hora.txt","w")
+        text_file.write("%s:%s:%s" %(Hora, Mins, Secs))
+        text_file.close()
+
+        text_file=open("Latitud.txt","w")
+        text_file.write("%s" %(lat))
+        text_file.close()
+
+        text_file=open("Longitud.txt","w")
+        text_file.write("%s" %(long))
+        text_file.close()
+
+        print("Tamaño de mensaje: 63")
+        print("Fecha:             %s/%s/%s" %(Anio,Mes,Dia))
+        print("Hora:              %s:%s:%s" %(Hora, Mins, Secs))
+        print("Latitud:           %s" % lat)
+        print("Longitud:         %s" % long)
+
+        fecha_db="%s/%s/%s" %(Anio,Mes,Dia)
+        hora_db="%s:%s:%s" %(Hora, Mins, Secs)
+        
+        db = mysql.connector.connect(user='root',database='disenouninorte')
+        cursor = db.cursor()
+        if abs(titud-latnew)>0 or abs(gtitud-longnew)>0 or abs(minutes-minnew)>0 :
+              cursor.execute("INSERT INTO coordenadas (Fecha,Hora,Latitud,Longitud) VALUES('%s','%s','%s','%s')" % (fecha_db,hora_db,lat,long))
+              latnew=titud
+              longnew=gtitud
+              minnew=minutes
+              print("Data Uploaded to DB")
+        db.commit()
+        cursor.close()
+        db.close()
+        
+
+    else:
+        print("Mensaje Corrupto")
---
+++ index.php
@@ -0,0 +1,73 @@
+<html class="no-js">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Diseño Uninorte 201630</title>
+	 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDsFelqvBHeiBZ9KEmCJ31cVXQjdo0aASk" async defer></script>
+	 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+     <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+	<style>
+		html,body {
+			height: 100%;
+        	margin: 0 auto;
+        	padding: 0;
+        } 
+      	#map{
+        width: 1000px;
+        height: 500px;
+      	}
+    </style>
+	</head>
+	<body>
+		<center>
+			<font face="helvetica">
+				<div id="container">
+			  		<!-- <table border="3">
+			  			<thead>
+				            <tr>
+				            	<th>  Hora  </th>
+				                <th>  Fecha  </th>
+				                <th>  Latitud  </th>
+				                <th>  Longitud  </th>
+				            </tr>
+				        </thead>
+						<tbody>
+					        <tr>
+					            <td style="width:100px"> <center id='main1'></center> </td>
+					            <td style="width:100px"> <center id='main2'></center> </td>
+					            <td style="width:100px"> <center id='main3'></center> </td>
+					            <td style="width:100px"> <center id='main4'></center> </td>
+					        </tr>
+					    </tbody>
+					</table> -->
+				</div>  
+			   <!--  <br></br>
+				<br></br> -->
+				<br></br>
+				<table border="3">
+					<thead>
+		          		<tr>
+							 <th>  MAPA  </th>
+		          		</tr>
+					</thead>
+					<tbody>
+						<tr>
+				             <td> <center id='map'></center> </td>            
+				        </tr>
+					</tbody>
+				</table>
+				<h2>Diseño Electrónico</h2>
+				<p><strong>Presentado por:</strong>
+				<br>Hernan Carrillo 
+				<br>Cristian Cepeda 
+				<br>Juan Diego Cotes
+				<br>Andrea Fontalvo 
+				<br>
+				<br><strong>Universidad del Norte
+				<br>2016 </strong></p>
+				<br></br>
+			</font>
+		</center>
+		<script src="js/realtime.js"></script>
+	</body>
+</html>
