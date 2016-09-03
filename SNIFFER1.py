5fea81527dce5038adea28a9be89233382995be4
jdcotes <jdcotes@uninorte.edu.co>
2016-09-03 00:06:08 -0500

    Actualizacion 0.52
    Timestamp prueba

--- SNIFFER1.py
+++ SNIFFER1.py
@@ -1,6 +1,7 @@
 import socket
 import datetime
 import MySQLdb
+import time
 
 IP = "172.31.16.250"
 PORT = 9000
@@ -47,10 +48,21 @@ while True:
         lat=str(titud)
     #   long = str(float(data[28:35])/-100000) 
     #   gtitud=float(data[28:35])/-100000
-        gtitud=float(data[28:35])/100000
+        gtitud=float(data[28:35])/-100000
         long=str(gtitud)
 
- 
+
+        f= 'Y-%m-%d %H:%M%S'
+        numweeks= float(data[7:11])
+        dayofweek=float(data[11])
+        ref= datetime.datetime(1980, 1, 6)
+        delta= datetime.timedelta(weeks=numweeks, days= dayofweek )
+        actualdate=ref + delta
+        secsincemidnight= float(data[11:16])
+        actualtime= datetime.timedelta(seconds=secsincemidnight)-datetime.timedelta(hours=5)
+        t= actualdate + actualtime
+        tsql= t.strfime(f)
+        tsql2= str(tsql)
 
         fecha_db="%s/%s/%s" %(Anio,Mes,Dia)
         hora_db="%s:%s:%s" %(Hora, Mins, Secs)
@@ -58,7 +70,7 @@ while True:
         db = MySQLdb.connect(host='localhost',user='root',passwd='1234',db='disenouninorte')
         cursor = db.cursor()
         if abs(titud-latnew)>0 or abs(gtitud-longnew)>0 or abs(minutes-minnew)>0 :
-              cursor.execute("INSERT INTO coordenadas (Fecha,Hora,Latitud,Longitud) VALUES('%s','%s','%s','%s')" % (fecha_db,hora_db,lat,long))
+              cursor.execute("INSERT INTO coordenadas (Fecha,Hora,Latitud,Longitud) VALUES('%s','%s','%s','%s')" % (fecha_db,tsql2,lat,long))
               latnew=titud
               longnew=gtitud
               minnew=minutes
