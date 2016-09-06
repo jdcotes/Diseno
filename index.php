b38cc627be3d02aed1736f0e14ce503b7a761001
jdcotes <jdcotes@uninorte.edu.co>
2016-09-06 14:22:58 -0500

    Actualizacion 0.6
    Cambio index

--- index.php
+++ index.php
@@ -6,6 +6,88 @@
 	 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDsFelqvBHeiBZ9KEmCJ31cVXQjdo0aASk" async defer></script>
 	 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
      <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+     <link rel="stylesheet" href="css/estilo.css">
+
+	 <style type="text/css">
+	 *{
+	 	margin:0;
+	 	padding:0;
+	 }
+	 body{
+	 	background:#e6e6e6;
+	 }
+
+	 h2, h3{
+	 	color:#B40404;
+	 }
+	 
+	 header, .main, footer{
+	 	width: 90%;
+	 	margin:auto;
+	 	max-width: 1000px;
+	 	margin: 20px auto;
+	 }
+
+	 header nav{
+	 	background: #0B0B61;
+	 	overflow:hidden;
+	 }
+
+	 header nav ul{
+	 	list-style:none;
+	 }
+
+	 header nav ul li{
+	 	float:left;
+	 }
+
+	 header nav ul li a{
+	 	padding:10px 20px;
+	 	display:block;
+	 	color:#fff;
+	 	text-decoration:none;
+	 }
+
+	 header nav ul li a:hover{
+	 	background:#0431B4;
+	 }
+
+	 .main .articles{
+	 	width: 68.5%;
+	 	margin-right:1.5%;
+	 	float:left;
+	 }
+
+	 .main .articles article{
+	 	padding: 20px;
+	 	background: #fff;
+	 	margin-bottom: 20px;
+	 }
+
+
+	 aside{
+	 	width:30%;
+	 	background: #D8D8D8;
+	 	float: left;
+	 	padding: 20px;
+	 	box-sizing: border-box;
+	 }
+
+	 footer{
+	 	background: #610B0B;
+	 	color:#D8D8D8;
+	 	clear:both;
+	 	padding:10px 0px;
+	 	text-align:center;
+	 }
+	  .main .map{
+	  	width: 90%;
+
+	  }
+
+
+	</style>
+		
 	<style>
 		html,body {
 			height: 100%;
@@ -13,50 +95,48 @@
         	padding: 0;
         } 
       	#map{
-        width: 1000px;
+        width: 100%;
         height: 500px;
       	}
     </style>
 	</head>
+
 	<body>
-		<center>
-			<font face="helvetica">
-				<div id="container">
-			  		<!-- <table border="3">
-			  			<thead>
-				            <tr>
-				            	<th>  Hora  </th>
-				                <th>  Fecha  </th>
-				                <th>  Latitud  </th>
-				                <th>  Longitud  </th>
-				            </tr>
-				        </thead>
-						<tbody>
-					        <tr>
-					            <td style="width:100px"> <center id='main1'></center> </td>
-					            <td style="width:100px"> <center id='main2'></center> </td>
-					            <td style="width:100px"> <center id='main3'></center> </td>
-					            <td style="width:100px"> <center id='main4'></center> </td>
-					        </tr>
-					    </tbody>
-					</table> -->
-				</div>  
-			   <!--  <br></br>
-				<br></br> -->
-				<br></br>
-				<table border="3">
-					<thead>
-		          		<tr>
-							 <th>  MAPA  </th>
-		          		</tr>
-					</thead>
-					<tbody>
-						<tr>
-				             <td> <center id='map'></center> </td>            
-				        </tr>
-					</tbody>
-				</table>
-				<h2>Diseño Electrónico</h2>
+		<font face="helvetica">
+
+<header>
+	<div class="logo">
+		<img src="logo.PNG">
+	</div>
+	<nav>
+		<ul>
+			<li><a href="http://instagram.com">Probando</a></li>
+			<li><a href="Indexx.html">Historico</a></li>
+			<li><a href="#">Acerca de Nosotros</a></li>
+		</ul>
+	</nav>
+</header>		
+<section class="main">
+<section class="articles">
+<article>
+	<h2>¿Donde está mi vehiculo?</h2>
+	<br>
+	<p>ist ein einfacher Demo-Text für die Print- und Schriftindustrie. Lorem Ipsum ist in der Industrie bereits der Standard Demo-Text seit 1500, als ein unbekannter Schriftsteller einhunderte überlebt, sondern auch in Spruch in die elektronische Schriftbearbeitung </p>
+	</article>
+
+    <article>
+    <div id='map'> </div>
+	</article>
+ </section>
+
+<aside>
+	<h3>Ultima ubicación</h3>
+	<br>
+	<p>ist ein einfacher Demo-Text für die Print- und Schriftindustrie. Lorem Ipsum ist in der Industrie bereits der Standard Demo-Text seit 1500, als ein unbekannter Schriftsteller einhunderte überlebt, sondern auch in Spruch in die elektronische Schriftbearbeitung gescha. Bekannt wurde es 1hunderte überlebt, sondern auch in Spruch in die elektronische Schriftbearbeitung geschafft (bemerke, nahezu unverändert). Bekannt wurde es 1</p>
+</aside>
+
+             	<!-- <div id='map'> </div>					
+				<p>Diseño Electrónico</p>
 				<p><strong>Presentado por:</strong>
 				<br>Hernan Carrillo 
 				<br>Cristian Cepeda 
@@ -66,8 +146,14 @@
 				<br><strong>Universidad del Norte
 				<br>2016 </strong></p>
 				<br></br>
-			</font>
-		</center>
+				</div>
+ -->
+ 
+</section>
+
+<footer>
+	<p> Diseño Electrónico - Universidad del Norte - 2016</p>
+</footer>
 		<script src="js/realtime.js"></script>
 	</body>
 </html>
