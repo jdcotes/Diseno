2be22a8601be25a466cd9b24a015e49b7e928776
jdcotes <jdcotes@uninorte.edu.co>
2016-09-10 16:17:14 -0500

    coordenadas

--- server/Android_Coordenadas.php
+++ server/Android_Coordenadas.php
@@ -4,15 +4,26 @@ $server = "localhost";
   $username = "root";
   $password = "1234";
   $database = "disenouninorte";
-  $con = @mysql_connect($server,$username,$password) or die("No se encontro el servidor");
-  mysql_select_db($database,$con)or die("No se encontro la base de datos");
+  $con = @mysql_connect($server,$username,$password) or die("No se encontró el servidor");
+  mysql_select_db($database,$con)or die("No se encontró la base de datos");
 
 date_default_timezone_set('America/Bogota');
 $fecha_servidor = date('Y-m-d H:i:s');
 
 
+$lat=0;
+$long=0;
+
+
+while true {
+
+$floatlat  = floatval($_POST[Latitud_gps]);
+$floatlong = floatval($_POST[Longitud_gps]);
+
+if ((abs($floatlat-$lat)>=0.0001) || (abs($floatlong-$long)>=0.0001) ) { 
+
 $consulta=mysql_query("INSERT INTO coordenadas (FechaGPS,FechaServer,Latitud,Longitud) VALUES('$_POST[Fecha_Hora_gps]','$fecha_servidor','$_POST[Latitud_gps]','$_POST[Longitud_gps]')");
 mysql_free_result($consulta);
-mysql_close($con);
 
-?>
+$lat=$floatlat;
+$long=$floatlong;
