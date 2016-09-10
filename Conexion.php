e97453284a4539cbf1bcfaf1789f9e1de67ff61a
jdcotes <jdcotes@uninorte.edu.co>
2016-09-10 14:57:10 -0500

    Actualizacion

--- Conexion.php
+++ Conexion.php
@@ -14,13 +14,13 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT Fecha, Hora FROM `coordenadas` order by fecha DESC limit 1";
+$sql = "SELECT FechaGPS FROM `coordenadas` order by FechaGPS DESC limit 1";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "Fecha: " . $row["Fecha"]. " - Hora: " . $row["Hora"]. "<br>";
+        echo "" . $row["FechaGPS"]"<br>";
     }
 } else {
     echo "0 results";
