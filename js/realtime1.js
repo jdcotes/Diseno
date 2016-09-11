debc6bb2f155e6dd6231107a9a71860004247001
jdcotes <jdcotes@uninorte.edu.co>
2016-09-11 15:37:24 -0500

    realtime 1 historicos

--- js/realtime1.js
+++ js/realtime1.js
@@ -1,15 +1,15 @@
 			
 var intevalo1;
 var entro=0;
- var map2;
-   var Tabla_MySql;
- //intevalo1 = setInterval('mapa1()',11000);
- intevalo1 = setInterval(function(){initMap()},30000);
-        // mapa1();
+var map2;
+var Tabla_MySql;
+intevalo1 = setInterval(function(){initMap()},30000);
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 10.963889, lng: -74.796389},
-    zoom: 15
-  });
+	if(entro==0){
+      map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 10.963889, lng: -74.796389},
+      zoom: 15}
+    });
+ entro=1;
 }
