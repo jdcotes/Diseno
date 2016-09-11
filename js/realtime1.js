13f1c68b4f1d25e494d653f805af164b7af5927b
jdcotes <jdcotes@uninorte.edu.co>
2016-09-11 17:50:39 -0500

    jdco
    historicos

--- js/realtime1.js
+++ js/realtime1.js
@@ -6,10 +6,18 @@ var Tabla_MySql;
 intevalo1 = setInterval(function(){initMap()},5000);
 var map;
 function initMap() {
-	if(entro==0){
-      map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 10.963889, lng: -74.796389},
-      zoom: 15});
-    }
- entro=1;
+	$.post("server/vivo.php",function(respuesta) {
+     /*bueno para que accedan a cada una de las filas de las tablas es así:*/
+     Tabla_MySql = JSON.parse(respuesta);
+   	 console.log(Tabla_MySql[0].Latitud);
+   	 console.log(Tabla_MySql[0].Longitud);
+   	 // console.log(Tabla_MySql[0].Latitud);
+   	 // console.log(Tabla_MySql[0].Longitud);
+	  if(entro==0){
+          map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 10.963889, lng: -74.796389},
+          zoom: 15});
+        }
+     entro=1;
+   });
 }
