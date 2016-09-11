e321380e24fbfbf71b0bc7de880ccd0cd809d30b
jdcotes <jdcotes@uninorte.edu.co>
2016-09-10 20:57:04 -0500

    probando substring query

--- Conexion.php
+++ Conexion.php
@@ -14,13 +14,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT FechaGPS FROM `coordenadas` order by FechaGPS DESC limit 1";
+$sql = "SELECT SUBSTRING( FechaGPS, 1, 10 ) AS FECHA 
+FROM coordenadas
+ORDER BY FechaGPS DESC 
+LIMIT 1";
+
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "" . $row["FechaGPS"]. " - Hora: " . $row["FechaGPS"]. "<br>";
+        echo "" . $row["FECHA"]. " - Hora: " . $row["FECHA"]. "<br>";
     }
 } else {
     echo "0 results";
