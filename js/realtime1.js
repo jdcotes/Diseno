36491c42b1519bad70ffaec25aefae5a55bc1917
jdcotes <jdcotes@uninorte.edu.co>
2016-09-13 16:15:42 -0500

    depuracion a

--- js/realtime1.js
+++ js/realtime1.js
@@ -13,7 +13,7 @@ var verificar = 0;
 var patha = null;   // Evitar repintado
 var latold = 0;
 var longold = 0;
-var i = 0;
+var a = 0;
 map2 = new google.maps.Map(document.getElementById('map'), {
 center:{lat: 11.01999, lng: -74.8509},
 zoom: 15});
@@ -71,13 +71,13 @@ function initMap() {
            //var prueba = lat-latold;
            //var hola = Math.abs(prueba)
            //console.log(hola);
-          console.log(i);
+          console.log(a);
           if ((Math.abs(lat-latold)>0.0003) || (Math.abs(lon-longold)>0.0003)){
-            routes2[i] = new google.maps.LatLng(lat,lon);
+            routes2[a] = new google.maps.LatLng(lat,lon);
             latold=lat;
             longold=lon;
             i = i + 1;
-            console.log(i);
+            console.log(a);
           }
           
            j=j+1;
