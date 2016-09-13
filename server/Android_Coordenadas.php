74ca137e02a9cefdb5d69816c7c3d8e1d01e14f5
jdcotes <jdcotes@uninorte.edu.co>
2016-09-13 11:58:50 -0500

    prueba envio datos

--- server/Android_Coordenadas.php
+++ server/Android_Coordenadas.php
@@ -1,37 +1,32 @@
 <?php
 
-$server = "localhost";
-  $username = "root";
-  $password = "1234";
-  $database = "disenouninorte";
-  $con = @mysql_connect($server,$username,$password) or die("No se encontró el servidor");
+ $server = "localhost";
+ $username = "root";
+ $password = "1234";
+ $database = "disenouninorte";
+ $con = @mysql_connect($server,$username,$password) or die("No se encontró el servidor");
   mysql_select_db($database,$con)or die("No se encontró la base de datos");
 
-date_default_timezone_set('America/Bogota');
-$fecha_servidor = date('Y-m-d H:i:s');
+  date_default_timezone_set('America/Bogota');
+  $fecha_servidor = date('Y-m-d H:i:s');
+
+  $floatlat  = floatval($_POST['Latitud_gps']);
+  $floatlong = floatval($_POST['Longitud_gps']);
+
+  $sql = "SELECT Latitud, Longitud FROM coordenadas ORDER BY ID DESC LIMIT 1"  ;
+  $ejecutar_sql1=mysql_query($sql) or die("Problemas en consulta: ".mysql_error());
+
+  
+  $reg = mysql_fetch_row($ejecutar_sql1);
+  $lat = floatval($reg[0]);
+  $long = floatval($reg[1]);
+  
 
 
+  if (( abs($floatlat-$lat)>=0.0001) || abs($floatlong-$long)>=0.0001)) { 
 
-$lat=0;
-$long=0;
-
-
-while (true) {
-
-$floatlat  = floatval($_POST['Latitud_gps']);
-$floatlong = floatval($_POST['Longitud_gps']);
-
-if (($floatlat-$lat)>=0.0003 || ($floatlong-$long)>=0.0003 || ($lat-$floatlat)>=0.0003 || ($long-$floatlong)>=0.0003  ) { 
-
-$consulta=mysql_query("INSERT INTO coordenadas (FechaGPS,FechaServer,Latitud,Longitud) VALUES('$_POST[Fecha_Hora_gps]','$fecha_servidor','$_POST[Latitud_gps]','$_POST[Longitud_gps]')");
-mysql_free_result($consulta);
-
-$lat=$floatlat;
-$long=$floatlong;
-
-}
-}
-
-mysql_close($con);
-
+    $consulta=mysql_query("INSERT INTO coordenadas (FechaGPS,FechaServer,Latitud,Longitud) VALUES('$_POST[Fecha_Hora_gps]','$fecha_servidor','$_POST[Latitud_gps]','$_POST[Longitud_gps]')");
+   mysql_free_result($consulta);
+  }
+  mysql_close($con);
 ?>
