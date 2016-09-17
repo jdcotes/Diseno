450b93cad74c48f911568b2024764be672259dba
jdcotes <jdcotes@uninorte.edu.co>
2016-09-17 10:05:57 -0500

    basedatos

--- server/Android_Coordenadas.php
+++ server/Android_Coordenadas.php
@@ -1,18 +1,38 @@
 <?php
 
-  $server = "localhost";
+$server = "localhost";
   $username = "root";
   $password = "1234";
   $database = "disenouninorte";
   $con = @mysql_connect($server,$username,$password) or die("No se encontró el servidor");
   mysql_select_db($database,$con)or die("No se encontró la base de datos");
 
-	date_default_timezone_set('America/Bogota');
-	$fecha_servidor = date('Y-m-d H:i:s');
+date_default_timezone_set('America/Bogota');
+$fecha_servidor = date('Y-m-d H:i:s');
 
 
-	$consulta=mysql_query("INSERT INTO coordenadas (FechaGPS,FechaServer,Latitud,Longitud) VALUES('$_POST[Fecha_Hora_gps]','$fecha_servidor','$_POST[Latitud_gps]','$_POST[Longitud_gps]')");
-	mysql_free_result($consulta);
+$lat=0;
+$long=0;
 
-	mysql_close($con);
+
+while true {
+
+$floatlat  = floatval($_POST[Latitud_gps]);
+$floatlong = floatval($_POST[Longitud_gps]);
+
+if ((abs($floatlat-$lat)>=0.0001) || (abs($floatlong-$long)>=0.0001) ) { 
+
+$consulta=mysql_query("INSERT INTO coordenadas (FechaGPS,FechaServer,Latitud,Longitud) VALUES('$_POST[Fecha_Hora_gps]','$fecha_servidor','$_POST[Latitud_gps]','$_POST[Longitud_gps]')");
+mysql_free_result($consulta);
+
+$lat=$floatlat;
+$long=$floatlong;
+
+
+}
+}
+
+
+mysql_close($con);
+
 ?>
