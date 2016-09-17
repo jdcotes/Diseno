919c5c8999e92f192f954a34f2a985935f4ea138
jdcotes <jdcotes@uninorte.edu.co>
2016-09-17 10:15:21 -0500

    catedra base de datos

--- server/Android_Coordenadas.php
+++ server/Android_Coordenadas.php
@@ -11,28 +11,10 @@ date_default_timezone_set('America/Bogota');
 $fecha_servidor = date('Y-m-d H:i:s');
 
 
-$lat=0;
-$long=0;
-
-
-while true {
-
-$floatlat  = floatval($_POST[Latitud_gps]);
-$floatlong = floatval($_POST[Longitud_gps]);
-
-if ((abs($floatlat-$lat)>=0.0001) || (abs($floatlong-$long)>=0.0001) ) { 
-
 $consulta=mysql_query("INSERT INTO coordenadas (FechaGPS,FechaServer,Latitud,Longitud) VALUES('$_POST[Fecha_Hora_gps]','$fecha_servidor','$_POST[Latitud_gps]','$_POST[Longitud_gps]')");
+
+
 mysql_free_result($consulta);
-
-$lat=$floatlat;
-$long=$floatlong;
-
-
-}
-}
-
-
 mysql_close($con);
 
 ?>
