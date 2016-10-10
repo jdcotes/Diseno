118afd3853ee190bf46e8ba288ca5d452b0d9166
jdcotes <jdcotes@uninorte.edu.co>
2016-10-09 23:48:01 -0500

    este si lo crea

--- index1.php
+++ index1.php
@@ -116,11 +116,9 @@
 								    	var radioInput = document.createElement('input');
 										radioInput.setAttribute('type', 'radio');
 										radioInput.setAttribute('name', name);
-
-										var btn = document.createElement("BUTTON");        // Create a <button> element
-										var t = document.createTextNode("CLICK ME");       // Create a text node
-										btn.appendChild(t);                                // Append the text to <button>
 										document.body.appendChild(btn);
+
+										
 								    </script>
 							</form>
 
