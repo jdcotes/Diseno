5cd4e4bd36320f98bc698b431438486bd106a922
jdcotes <jdcotes@uninorte.edu.co>
2016-10-09 23:45:29 -0500

    botton

--- index1.php
+++ index1.php
@@ -116,6 +116,11 @@
 								    	var radioInput = document.createElement('input');
 										radioInput.setAttribute('type', 'radio');
 										radioInput.setAttribute('name', name);
+
+										var btn = document.createElement("BUTTON");        // Create a <button> element
+										var t = document.createTextNode("CLICK ME");       // Create a text node
+										btn.appendChild(t);                                // Append the text to <button>
+										document.body.appendChild(btn);
 								    </script>
 							</form>
 
