e59a40b8ac97d7d81bfb630da12afc2513302783
jdcotes <jdcotes@uninorte.edu.co>
2016-10-09 19:51:03 -0500

    creacion tabla

--- index5.php
+++ index5.php
@@ -1,12 +1,14 @@
 <!DOCTYPE HTML>
 <html class = "no-js">
 	<table>
-
+        <tr>
 		Marca: <input id = "marca">
-	
+		</tr>
+		<tr>
 		Referencia: <input id = "referencia">
-	
+		</tr>
+		<tr>
 		Modelo:  <input id = "modelo">
-	
+		</tr>
 	</table>
 </html>
