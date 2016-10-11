1d55bdefa890bb7d9e7db25bcd28df99d42a2794
jdcotes <jdcotes@uninorte.edu.co>
2016-10-10 23:19:30 -0500

    malana

--- index.php
+++ index.php
@@ -137,7 +137,7 @@
                 var auto_refresh = setInterval(function (){
                 
                 	while (i<tamano){
-                   	$('#FECHA').load('LLAMAR.php',{id:vehi[i]}).fadeIn("slow");
+                   	$('#FECHA'+i+'').load('LLAMAR.php',{id:vehi[i]}).fadeIn("slow");
                  	}
                  	}, 5000);
                 }				
