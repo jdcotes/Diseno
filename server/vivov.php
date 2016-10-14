284381ac0c5ca35bfb96a4eaa21f8103f3a1664d
andreafontalvoe <echavezc@uninorte.edu.co>
2016-10-13 21:09:43 -0500

    nada

--- server/vivov.php
+++ server/vivov.php
@@ -6,11 +6,9 @@
   $con = @mysql_connect($server,$username,$password) or die("No se encontró el servidor");
   mysql_select_db($database,$con)or die("No se encontró la base de datos");
   
-
   $um = $_POST['mark'];
   $dois =$_POST['refrence'];
   $treis =$_POST['model'];
-
   
   $sql = "INSERT INTO registro (Marca, Referencia, Modelo) VALUES ('$um','$dois','$treis')";
   $ejecutar_sql1=mysql_query($sql) or die("Problemas en consulta: ".mysql_error());
