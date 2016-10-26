14f221f419dc9540a400d41ff64708b525457617
andreafontalvoe <echavezc@uninorte.edu.co>
2016-10-26 02:52:12 -0500

    probando medicion

--- Conexion1.php
+++ Conexion1.php
@@ -22,7 +22,7 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
     	
-        echo " ". $row["velocidad"] . ;
+        echo "La velocidad del vehículo es: " . $row["velocidad"] . "<br>" ;
     }
 } else {
     echo "No hay medicion registrada";
--- index.php
+++ index.php
@@ -89,7 +89,7 @@
                                         }, 5000); // refresh every 5 seconds
                                     </script>
 
-								<div>La velocidad del vehículo es: <div id="medicion"></div></div>
+								<div id="medicion"></div>
 								<div id='map'>
 							</section>
 						</div>
