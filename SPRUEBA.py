afc50663f9c8380ab804c6a3ae8ad3f3fa7e7409
jdcotes <jdcotes@uninorte.edu.co>
2016-10-29 11:58:29 -0500

    nuevo sniffer

---
+++ SPRUEBA.py
@@ -0,0 +1,22 @@
+import socket
+import datetime
+import MySQLdb
+import time
+
+IP = "172.31.16.250"
+PORT = 9000
+numero_mensaje = 1
+latnew=0
+longnew=0
+minnew=0
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+sock.bind((IP,PORT))
+
+while True:
+
+    data,addr=sock.recvfrom(1024)
+    print(data)
+
+    
+    
