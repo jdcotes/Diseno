a5b24c12c4a82eb69015afcc6c836d0581f61e7a
jdcotes <jdcotes@uninorte.edu.co>
2016-11-02 01:06:27 -0500

    acercate

--- Conexion1.php
+++ Conexion1.php
@@ -14,19 +14,19 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT Velocidad FROM  `coordenadas` ORDER BY ID DESC LIMIT 1";
+$sql = "SELECT Velocidad FROM  `coordenadas`WHERE ID = '1' ORDER BY ID DESC LIMIT 1";
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-    		if ($row["Velocidad"] == '22'){
+    		if ($row["Velocidad"] == '11'){
         // echo "<h4>La velocidad del vehículo es:</h4><h2>" . $row["Velocidad"] . " Km/h</h2>" ;
-				echo "<h4> Puerta del contenedor abierta </h4>";
+				echo "<b><h3> Puerta del contenedor abierta </h3></b>";
 				}
-				if ($row["Velocidad"] == '11'){
-				echo "<h4> Puerta del contenedor cerrada </h4>";
+				if ($row["Velocidad"] == '22'){
+				echo "<b><h3> Puerta del contenedor cerrada </h3></b>";
 				}
     }
 } else {
